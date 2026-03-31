73998deb3b80c88335254f2657d7b04c105d2fc2
hankyul <developer.kyul@gmail.com>
2026-03-31 17:16:11 +0900

    learn. Next.js 매뉴얼 인스톨레이션

---
+++ 01-Next.js-시작하기/01-메뉴얼-인스톨레이션/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <header className="flex min-h-screen flex-col items-center justify-center bg-slate-50">
+      <h1 className="text-4xl font-extralight text-slate-950">안녕 Next.js</h1>
+      <img src="/vercel.svg" alt="Vercel 로고" className="mt-2 size-8" />
+    </header>
+  )
+}
