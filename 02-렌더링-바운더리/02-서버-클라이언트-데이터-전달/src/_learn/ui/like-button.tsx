6e00b60fe6a985231df6dcf9b914b27cbaec49e8
hankyul <developer.kyul@gmail.com>
2026-04-01 16:37:24 +0900

    learn. 클라이언트 컴포넌트에서 서버 함수 실행 - 'use server' 디렉티브(지시어) 파일 상단에 선언 - 내부적으로 서버 함수는 사용자 요청을 서버에 보내 서버에서 실행됨

---
+++ 02-렌더링-바운더리/02-서버-클라이언트-데이터-전달/src/_learn/ui/like-button.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import { useState } from 'react'
+import { cn, isErrorObject, wait } from '@/utils'
+import { writeLikes } from '@/functions/likes-read-write' // 서버 함수
+
+interface Props {
+  initialLikes: number
+}
+
+export default function LikeButton({ initialLikes }: Props) {
+  const [likes, setLikes] = useState(initialLikes)
+  const [isPending, setIsPending] = useState(false)
+
+  const handleLike = async () => {
+    console.log(isPending)
+
+    if (isPending) {
+      console.log('사용자의 클릭 연타 방지 (방어)')
+      return
+    }
+
+    setIsPending(true)
+
+    try {
+      const nextCount = likes + 1
+
+      // 서버 함수 호출 (서버의 JSON 파일을 업데이트)
+      // const result = { success: true }
+      const result = await writeLikes(nextCount)
+
+      await wait(2000) // 서버 응답 대기 시간 시뮬레이션
+      // 서버 함수 결과에 따라 상태 업데이트
+      if (result.success) setLikes(nextCount)
+    } catch (error) {
+      if (isErrorObject(error)) console.error(error)
+      else console.error('[에러 발생]', String(error))
+    } finally {
+      setIsPending(false)
+    }
+  }
+
+  return (
+    <button
+      type="button"
+      aria-disabled={isPending}
+      className={cn(
+        'cursor-pointer active:scale-97',
+        'flex items-center gap-2',
+        'rounded-full border border-rose-200',
+        'bg-rose-50 px-6 py-2 text-rose-600',
+        'transition-all hover:bg-rose-100 disabled:opacity-50',
+      )}
+      onClick={handleLike}
+    >
+      <span>{isPending ? '⏳' : '❤️'}</span>
+      <span className="font-bold">좋아요 {likes}</span>
+    </button>
+  )
+}
---
+++ 02-렌더링-바운더리/02-서버-클라이언트-데이터-전달/src/_learn/ui/passing-data.tsx
@@ -0,0 +1,46 @@
+import { readLikes } from '@/functions/likes-read-write' // 서버 함수 (Next.js 서버에서 실행)
+import LikeButton from './like-button' // 클라이언트 컴포넌트
+
+// 1. 서버 컴포넌트 - (READ) 요청 -> 서버 함수
+// 2. 서버 컴포넌트 <- (DATA) 응답 - 서버 함수
+// 3. 서버 컴포넌트 - (DATA) 전달 - 클라이언트 컴포넌트
+// 4. 클라이언트 컴포넌트 - 전달된 (DATA)로 상태 초기화
+// 5. 클라이언트 컴포넌트 <- 사용자 상호작용 (click)
+// 6. 클라이언트 컴포넌트 - (UPDATE) 실행 -> 서버 함수 ('use server')
+// 7. 서버 함수 - (WRITE) 실행 -> likes.json 파일 수정
+// 8. 서버 컴포넌트 - 렌더링 (SERVER) -> 클라이언트 컴포넌트에 props 전달
+// 9. 클라이언트 컴포넌트 - 렌더링 (SERVER) -> HTML 생성, 전달받은 props를 토대로 JS 청크 생성
+// 10. JS 청크 - 전송 (BROWSER) -> 브라우저 실행 (Hydration, 수화)
+// 11. 서버 컴포넌트로부터 전달된 데이터 값으로 탣로 React에 의해 메모리 상에서 관리
+
+export default async function PassingData() {
+  // 서버 컴포넌트 (비동기 함수 사용 가능)
+
+  // 서버 함수 readLikes()를 실행해
+  // 결과 값을 클라이언트 컴포넌트에 전달
+  const currentLikes = await readLikes()
+
+  return (
+    <section className="flex flex-col items-center justify-center p-24">
+      <div className="rounded-2xl border border-gray-100 bg-white p-10 shadow-xl">
+        <h2 className="mb-4 text-2xl font-bold text-rose-600">
+          Next.js 데이터 전달
+        </h2>
+        <p className="mb-8 text-sm text-gray-600">
+          아래 버튼을 누르면 서버의
+          <code className="mx-0.5 rounded bg-gray-100 px-1.5 py-0.5 text-rose-600">
+            likes.json
+          </code>
+          파일이 업데이트됩니다.
+        </p>
+
+        {/* 클라이언트 컴포넌트에 데이터 전달(passing data) */}
+        <LikeButton initialLikes={currentLikes} />
+
+        <p className="mt-6 font-mono text-xs text-gray-400">
+          서버 사이드 데이터 = {currentLikes}
+        </p>
+      </div>
+    </section>
+  )
+}
