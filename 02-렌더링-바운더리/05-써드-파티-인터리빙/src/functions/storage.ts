e27b97c61cc44b2103f1473a49b0dd016e5d1bfc
hankyul <developer.kyul@gmail.com>
2026-04-03 11:15:52 +0900

    learn. 서버/클라이언트 전용 함수 (server-only / client-only)

--- 02-렌더링-바운더리/05-써드-파티-인터리빙/src/functions/storage.ts
+++ 02-렌더링-바운더리/05-써드-파티-인터리빙/src/functions/storage.ts
@@ -1,10 +1,11 @@
-// 클라이언트 전용 함수 (window는 서버에 없음)
+import 'client-only'
+
 export const getStorage = (key: string) => {
-  const data = window.localStorage.getItem(key)
+  const data = globalThis.localStorage.getItem(key)
   if (!data) return null
   return JSON.parse(data)
 }
 
 export const setStorage = <T>(key: string, value: T) => {
-  window.localStorage.setItem(key, JSON.stringify(value))
+  globalThis.localStorage.setItem(key, JSON.stringify(value))
 }
