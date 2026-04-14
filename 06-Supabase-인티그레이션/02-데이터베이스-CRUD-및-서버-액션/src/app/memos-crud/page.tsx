c149b55cb7263ec6458dfdf6e5d044a64b70929f
hankyul <developer.kyul@gmail.com>
2026-04-14 14:42:46 +0900

    learn. 에러 바운더리 (ErrorBoundary) - 라우트 세그먼트 내부에 error.tsx 파일 생성 - 오픈 소스 라이브러리 react-error-boundary

--- 06-Supabase-인티그레이션/02-데이터베이스-CRUD-및-서버-액션/src/app/memos-crud/page.tsx
+++ 06-Supabase-인티그레이션/02-데이터베이스-CRUD-및-서버-액션/src/app/memos-crud/page.tsx
@@ -1,13 +1,17 @@
+import { Suspense } from 'react'
+
+import { readMemoAction } from '@/actions/memo-actions'
+import { Spinner } from '@/components/ui/spinner'
 import MemoForm from './memo-form'
 import MemoList from './memo-list'
 
 export default function MemoCRUDPage() {
-
   /**
-   * readMemosAction 서버 액션을 정의합니다.
-   * readMemosAction 액션를 실행한 Promise를 MemoList 컴포넌트에 전달합니다.
+   * readMemoAction 서버 액션을 정의합니다. (Supabase 데이터 가져오기)
+   * readMemoAction 액션를 실행한 Promise를 MemoList 컴포넌트에 전달합니다.
    */
-  
+  const memolistPromise = readMemoAction() // Promise<ActionResponse<Memo[]>>
+
   return (
     <section className="mx-auto w-9/10 max-w-3xl px-6 py-12 antialiased lg:w-3/5">
       <header className="mb-10 flex items-center justify-between">
@@ -20,13 +24,14 @@ export default function MemoCRUDPage() {
           </p>
         </div>
       </header>
-            
+
       <div className="mb-12 rounded-3xl border-2 border-slate-100 bg-slate-50/50 p-6">
         <MemoForm />
       </div>
 
-      <MemoList />
-
+      <Suspense fallback={<Spinner />}>
+        <MemoList memolistPromise={memolistPromise} />
+      </Suspense>
     </section>
   )
 }
